1e9769e37811010b861fabfc2725f67d4df10a04
SebastianA66 <42424010+SebastianA66@users.noreply.github.com>
2018-11-16 16:03:06 +1100

    Initial commit

---
+++ Assets/Scripts/CharacterHandler.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterHandler : MonoBehaviour
+{
+    public bool alive;
+
+    public int skillPoint;
+
+    public float maxHealth;
+    public float curHealth;
+    public float maxMana;
+    public float curMana;
+    public float Defence;
+    public float Attack;
+    public float maxStam;
+    public float curStam;
+    public float Speed;    
+
+    public int Vitality;
+    public int Strength;
+    public int Intelligence;
+    public int Endurance;
+    public int Dexterity;
+    public int Resistance;
+
+    private void Start()
+    {
+        // Character starts alive
+        alive = true;
+        // current health starts off at max health
+        curHealth = maxHealth;
+        // Vitality increases max health 
+        maxHealth += Vitality * 3;
+        // Strength increases attack
+        Attack += Strength * 2;
+        // Intelligence increases max mana
+        maxMana += Intelligence * 3;
+        // Endurance increases max stamina
+        maxStam += Endurance * 2;
+        // Dexterity increases max speed
+        Speed += Dexterity * 1.5f;
+        // Resistance increases max defence
+        Defence += Resistance * 3;
+
+    }
+}
